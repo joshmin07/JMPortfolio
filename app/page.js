c4eedebe8a7e787ce39054e406461df0a6d332cc
joshmin07 <joshuaminervini123@gmail.com>
2026-01-13 12:37:18 -0500

    Update image paths to use JMPortfolio directory
    Changed image paths in project and about sections from '/JoshMin2k4Website/' to '/JMPortfolio/' to reflect updated directory structure.

--- app/page.js
+++ app/page.js
@@ -122,9 +122,9 @@ export default function Home() {
       discordLink: "https://discord.com/invite/k6YFMyvG9C",
       githubLink: "https://github.com/joshmin07",
       images: [
-        "/JoshMin2k4Website/ProjectImage1Menu.png",
-        "/JoshMin2k4Website/ProjectImage2Save.png",
-        "/JoshMin2k4Website/ProjectImage3Game.gif"
+        "/JMPortfolio/ProjectImage1Menu.png",
+        "/JMPortfolio/ProjectImage2Save.png",
+        "/JMPortfolio/ProjectImage3Game.gif"
       ]
     },
     {
@@ -154,9 +154,9 @@ export default function Home() {
       description: "I have been passionate about technology ever since I was little, I sparked my interest in programming where I learned to code using Scratch and tinkering with GameMaker Studio. I then decided to take 3D and VR Game Development in Highschool where I learned my first programming language C#. Here I am now! Building a career in this amazing field and learning everyday!",
       technologies: ["Enthusiast", "Learner", "Developer", "Designer", "Creative", "Hardworking"],
       images: [
-        "/JoshMin2k4Website/AboutMe1.jpg",
-        "/JoshMin2k4Website/AboutMe2.jpg",
-        "/JoshMin2k4Website/AboutMe3.jpg"
+        "/JMPortfolio/AboutMe1.jpg",
+        "/JMPortfolio/AboutMe2.jpg",
+        "/JMPortfolio/AboutMe3.jpg"
       ],
     },
   ];
